6d3694faee24513b88c60c8684681ceadb43df4f
yoni-Jo <yoni9258@naver.com>
2021-04-23 12:02:26 +0900

    click 이벤트 페이지

---
+++ fursys/js/click.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const showroomList=document.querySelector(".list");
+
+
+
+function list(e){
+    console.log(e.target)
+    if(e.target.){
+
+    }
+    
+}
+showroomList.addEventListener("click",list);
