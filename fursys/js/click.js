48591aa43e91345d74101be358aedc1159ee7d9a
yoni-Jo <yoni9258@naver.com>
2021-04-23 14:35:26 +0900

    클릭시 해당 이미지 만 보이게

--- fursys/js/click.js
+++ fursys/js/click.js
@@ -1,14 +1,20 @@
-"use strict";
+const li = document.querySelectorAll(".list>li")
+const detail = document.querySelectorAll(".detail>li")
 
-const showroomList=document.querySelector(".list");
-
-
-
-function list(e){
-    console.log(e.target)
-    if(e.target.){
-
-    }
+li.forEach((lis, index) => {
+    const linum = index;
     
-}
-showroomList.addEventListener("click",list);
+    lis.style.zIndex="0"; 
+    lis.addEventListener("click", () => {
+        
+        const img = detail[index].lastElementChild
+        const imgnum = index
+        detail.forEach((details, index) => {
+
+        })
+        
+        if(linum==imgnum){
+            detail[linum].style.zIndex = "10";
+        }
+    })
+})
