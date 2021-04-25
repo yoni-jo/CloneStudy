9fb645e35aa05868edc15936b9d0f4bae8ce8da3
Yoni <yoni9258@naver.com>
2021-04-25 21:08:04 +0900

    클릭시opacity 0->1 trangition을 줘서 자연스럽게 사라졌다 나타나게 수저ㅏㅇ

--- fursys/js/click.js
+++ fursys/js/click.js
@@ -1,17 +1,20 @@
 const list_button = document.querySelectorAll(".list>li")
 const detail = document.querySelectorAll(".detail>li")
 
+
 list_button.forEach((list_buttons, index) => {
         list_buttons.addEventListener("click", (event) => {
                 event.preventDefault();
                 const img = detail[index];
 
                 detail.forEach((details) => {
-                        details.style.removeProperty("z-index")
+                        details.style.zIndex = "0"
+                        details.style.opacity = "0"
                 })
                 img.style.zIndex = '1'
-
-
+                img.style.opacity = '1'
 
         })
+
+
 })
