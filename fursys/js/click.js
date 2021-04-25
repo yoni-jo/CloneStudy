3a2071a39d9adfa7424be8863891b4f18070fd6d
Yoni <yoni9258@naver.com>
2021-04-25 18:26:37 +0900

    코드 더 간편하게

--- fursys/js/click.js
+++ fursys/js/click.js
@@ -1,20 +1,17 @@
-const li = document.querySelectorAll(".list>li")
+const list_button = document.querySelectorAll(".list>li")
 const detail = document.querySelectorAll(".detail>li")
 
-li.forEach((lis, index) => {
-    const linum = index;
-    
-    lis.style.zIndex="0"; 
-    lis.addEventListener("click", () => {
-        
-        const img = detail[index].lastElementChild
-        const imgnum = index
-        detail.forEach((details, index) => {
+list_button.forEach((list_buttons, index) => {
+        list_buttons.addEventListener("click", (event) => {
+                event.preventDefault();
+                const img = detail[index];
+
+                detail.forEach((details) => {
+                        details.style.removeProperty("z-index")
+                })
+                img.style.zIndex = '1'
+
+
 
         })
-        
-        if(linum==imgnum){
-            detail[linum].style.zIndex = "10";
-        }
-    })
 })
