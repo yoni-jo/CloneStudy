fc30723fafcda7c3893afb35a39a3486f578c0d2
Yoni <yoni9258@naver.com>
2021-06-13 20:36:06 +0900

    lodash 라이브러리를 사용 =>scroll이벤트 일정시간마다 발생 gsap 라이브러리사용 =>헤더의 높이값보다 커질경우 .badges영역 자연스럽게 사라지게 설정

--- #5-스타벅스/js/main.js
+++ #5-스타벅스/js/main.js
@@ -3,16 +3,40 @@
 const searchEl = document.querySelector(".search")
 const searchInput = searchEl.querySelector("input")
 
-searchEl.addEventListener("click", ()=>{
+searchEl.addEventListener("click", () => {
 
-    searchInput.focus()//클릭시 커서가 해당 위치로 
+    searchInput.focus() //클릭시 커서가 해당 위치로 
 })
 
-searchInput.addEventListener("focus",()=>{
+searchInput.addEventListener("focus", () => {
     searchEl.classList.add("focused")
-    searchInput.setAttribute('placeholder','통합검색')
+    searchInput.setAttribute('placeholder', '통합검색')
 })
-searchInput.addEventListener("blur",()=>{
+searchInput.addEventListener("blur", () => {
     searchEl.classList.remove("focused")
-    searchInput.setAttribute('placeholder','')
+    searchInput.setAttribute('placeholder', '')
 })
+
+
+const header = document.querySelector("header")
+const badges = document.querySelector(".badges")
+
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js" integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+window.addEventListener("scroll", _.throttle(function () {
+    console.log(window.scrollY)
+    if (window.scrollY > header.offsetHeight) {
+
+        gsap.to(badges, .2, {
+            opacity: 0,
+            display: 'none'
+        })
+
+    } else {
+        gsap.to(badges, .2, {
+            opacity: 1,
+            display: 'block'
+        })
+    }
+}, 300))
+
+//_,throttle(함수,시간)
