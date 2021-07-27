9a2e86a89b136643e29985704f4283a2e3a87cc6
Yoni <yoni9258@naver.com>
2021-07-27 19:57:55 +0900

    event : search버튼, 햄버거버튼 클릭이벤트

--- #9.LIRIKOS/js/index.js
+++ #9.LIRIKOS/js/index.js
@@ -4,23 +4,37 @@
 const search_Btn = document.querySelector(".search");
 const globalSearch = document.querySelector(".global__Search");
 const nav = document.querySelector(".nav")
-const close_Btn = document.querySelector('.close-btn')
+const close_Btn = document.querySelectorAll('.close-btn')
+const ham_Btn = document.querySelector('.ham-Btn')
+const global_Menu = document.querySelector(".global__Menu")
 
 let SLIDEEVENT;
-// header안 search 클릭시 
-function search(event) {
+const HIDE="hide"
 
+
+// header안 search 클릭시 
+function search() {
     SLIDEEVENT = 'slideDown'
-    nav.classList.toggle('hide')
+    nav.classList.add(HIDE)
     globalSearch.classList.add(SLIDEEVENT);
-    close_Btn.addEventListener("click", close)
 }
 search_Btn.addEventListener("click", search)
 
 
-
-function close(event) {
-    const parent = this.parentElement.parentElement
-    parent.classList.remove(SLIDEEVENT)
-    nav.classList.toggle('hide')
+//header 햄버거버튼 클릭시 
+function global() {
+    SLIDEEVENT = "slideLeft"
+    global_Menu.classList.add(SLIDEEVENT)
+    nav.classList.add(HIDE)
 }
+ham_Btn.addEventListener("click", global)
+
+// 닫힘버튼눌렀을때
+function close() {
+    const parent = this.parentElement
+    parent.classList.remove(SLIDEEVENT)
+    nav.classList.remove('hide')
+}
+close_Btn.forEach((closebtn) => {
+    closebtn.addEventListener("click", close)
+})
