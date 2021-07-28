90f0dc9eaa9b70d9d8dda0531bb6ed977b42e159
Yoni <yoni9258@naver.com>
2021-07-28 23:13:44 +0900

    event: 메인슬라이드 오토슬라이드 설정 및 progressbar js로 하단이동

--- #9.LIRIKOS/js/slide.js
+++ #9.LIRIKOS/js/slide.js
@@ -2,3 +2,32 @@
 
 
 // main slide
+new Swiper('.main__slide .swiper-container', {
+    direction: 'horizontal', //수평이동
+    speed: 1000,
+    loop: true, //반복설정
+    autoplay: {
+        delay: 3000
+    },
+    slidesPerView: 1, //보여줄 슬라이드 개수
+    centeredSlides: true,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    pagination: {
+        el: '.main__slide .swiper-pagination',
+        type: 'progressbar',
+        
+    },
+    navigation: {
+        prevEl: '.main__slide .swiper-button-prev',
+        nextEl: '.main__slide .swiper-button-next'
+    }
+});
+// progressbar 위치 하단이동
+const main__slide = document.querySelector(".main__slide")
+const swiper_Pagination =  main__slide.querySelector(".swiper-pagination")
+const TOP = "calc(100% - 4px)"
+
+swiper_Pagination.style.top =`${TOP}`
