0d0046812eb6ed85f2cf57a5e5ae6ef0052f4aae
Yoni <yoni9258@naver.com>
2021-08-01 17:16:41 +0900

    이벤트 : 오타수정

--- #9.LIRIKOS/js/bestItem.js
+++ #9.LIRIKOS/js/bestItem.js
@@ -2,26 +2,26 @@
 //상품리스트 이미지
 const item = [{
     img: 'FACIAL ERASER CLEANSING FORM.jpg',
-    hover: 'FACIAL-ERASER-CLEANSING-FORM-hover.jpg'
+    hover: 'FACIAL-ERASER-CLEANSING-FORM-HOVER.jpg'
 }, {
     img: 'DEEP SEA WATERFALL TONER.jpg',
-    hover: 'DEEP-SEA-WATERFALL-TONER-hover.jpg'
+    hover: 'DEEP-SEA-WATERFALL-TONER-HOVER.jpg'
 }, {
     img: 'FACIAL ERASER CLEANSING OIL FORM.jpg',
-    hover: 'FACIAL-ERASER-CLEANSING-OIL-FORM-hover.jpg'
+    hover: 'FACIAL-ERASER-CLEANSING-OIL-FORM-HOVER.jpg'
 }, {
     img: 'TRUE AMPOULE MASK.jpg',
-    hover: 'TRUE-AMPOULE-MASK-hover.jpg'
+    hover: 'TRUE-AMPOULE-MASK-HOVER.jpg'
 }, {
     img: '3-MINUTE CLAY MASK.jpg',
-    hover: '3-MINUTE-CLAY-MASK-hover.jpg'
+    hover: '3-MINUTE-CLAY-MASK-HOVER.jpg'
 }, {
     img: 'DEEP SEA WATERFALL CREAM.jpg',
-    hover: 'DEEP-SEA-WATERFALL-CREAM-hover.jpg'
+    hover: 'DEEP-SEA-WATERFALL-CREAM-HOVER.jpg'
 }]
 
 const best_sellers = document.querySelector(".best-sellers")
-const items = document.querySelector(".items")
+const items = best_sellers.querySelector(".items")
 const front_Img_el = items.querySelectorAll(".front")
 
 // 이미지파일 자동등록 
@@ -31,6 +31,6 @@ item.forEach((list, index) => {
     const figure_El = front_Img_el[index].parentNode
 
     front_Img_el[index].setAttribute('src', `./img/lirikos/best_sellers/${img}`)
-    figure_El.style['background-image'] = `url(../img/lirikos//best_sellers/${hover})`
+    figure_El.style['background-image'] = `url(./img/lirikos/best_sellers/${hover})`
 
 })
